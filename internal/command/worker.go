8a667717c7654bcf80a03f45f908f24f1ea189bd
Adrian PK <adrianpk@adrianpk.com>
2024-06-12 23:13:50 +0200

    Update copy command implementation

--- internal/command/worker.go
+++ internal/command/worker.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/adrianpk/snapfig/internal/config"
 	"github.com/spf13/afero"
@@ -9,11 +10,11 @@ import (
 
 // Worker is the command's companion struct that does the actual work.
 type Worker struct {
-	cfg     *config.Config
-	fs      afero.Fs
-	fsRoot  string
-	toolDir string
-	dirs    []string
+	cfg       *config.Config
+	fs        afero.Fs
+	fsRoot    string
+	configDir string
+	dirs      []string
 }
 
 func NewWorker(cfg *config.Config) *Worker {
@@ -22,6 +23,27 @@ func NewWorker(cfg *config.Config) *Worker {
 		fs:   afero.NewOsFs(),
 		dirs: []string{},
 	}
+}
+
+func (w *Worker) Setup() error {
+	if w.fsRoot == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		w.fsRoot = home
+	}
+
+	w.fs = afero.NewBasePathFs(afero.NewOsFs(), w.fsRoot)
+
+	toolConfigDir, err := w.ensureSnapfigDir()
+	if err != nil {
+		return err
+	}
+
+	w.configDir = toolConfigDir
+
+	return nil
 }
 
 // Cfg returns the ommand's configuration
